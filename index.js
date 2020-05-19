85b29d2171182d89ea2489714b3dab6169f1b792
rs30holl <rs30holl@siena.edu>
2020-05-19 19:24:27 -0400

    Test

---
+++ index.js
@@ -0,0 +1,11 @@
+function sendEmail(){
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "<rsh930@gmail.com>",
+        Password: "<Avengers930>",
+        To: '<rsh930@gmail.com>',
+        From: "<rsh930@gmail.com>",
+        Subject: "<New visit>",
+        Body: "<Someone clicked on your link.>",
+    });
+}
