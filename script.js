6ffbde08181132853d1727bc3cad6d160c37448a
ryan-holland <ryan.holland@mtxb2b.com>
2021-02-25 22:28:16 -0500

    popup feature branch intial

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ function showhide(whichDiv) {
     var div = document.getElementById(whichDiv);
     div.classList.toggle('hidden');
 }
+
+function showModal() {
+    var modal = document.getElementById("modal-1");
+    modal.classList.toggle('md-show');
+}
+
+function closeModal() {
+    var modal = document.getElementById("modal-1");
+    modal.classList.toggle('md-show');
+}
